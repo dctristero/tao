10ea6209f586204d25c157f41a8c26c7c651d720
dctristero <123331986+dctristero@users.noreply.github.com>
2023-12-12 16:27:06 -0600

    grid links

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,33 +1,42 @@
 import React from "react";
-import { ChakraProvider, Box, Flex, SimpleGrid, chakra } from "@chakra-ui/react";
+import {Link} from "react-router-dom"
+import {
+  ChakraProvider,
+  Box,
+  Flex,
+  SimpleGrid,
+  chakra,
+  Button
+} from "@chakra-ui/react";
 
+export default function Test() {
+  // Function to generate an array of numbers from 1 to 81
+  const makeLinks = () => {
+    const numbers = [];
+    for (let i = 1; i <= 81; i++) {
+      numbers.push(
+        <Button color={["primary.300"]} className="navbtn" variant="ghost">
+          <Link to={`${i}`}>{i}</Link>
+        </Button>);
+    }
+    return numbers;
+  };
 
-export default function Test(){
-
-    // Function to generate an array of numbers from 1 to 81
-    const generateNumbers = () => {
-      const numbers = [];
-      for (let i = 1; i <= 81; i++) {
-        numbers.push(i);
-      }
-      return numbers;
-    };
-
-    return (
-      <div>
-            <Flex
-      bg={["primary.100"]}
-      color={["primary.300"]}
-      p={7}
-      w="full"
-      h="full"
-      justifyContent="center"
-      alignItems="center"
-      //pos="absolute"
-    >
-        <Box p={4}>
-          <SimpleGrid columns={9} spacing={0}>
-            {generateNumbers().map((number) => (
+  return (
+    <div>
+      <Flex
+        bg={["primary.100"]}
+        color={["primary.300"]}
+        p={7}
+        w="full"
+        h="full"
+        justifyContent="center"
+        alignItems="center"
+        //pos="absolute"
+      >
+        <Box p={2}>
+          <SimpleGrid id="grid" columns={9} spacing={0}>
+            {makeLinks().map((number) => (
               <Box
                 key={number}
                 borderColor={["primary.300"]}
@@ -40,11 +49,10 @@ export default function Test(){
             ))}
           </SimpleGrid>
         </Box>
-        </Flex>
-        </div>
-    );
-
-};
+      </Flex>
+    </div>
+  );
+}
 
 // const GridWithNumbers = () => {
 //   // Function to generate an array of numbers from 1 to 81
