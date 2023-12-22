d1cf301800b854d5560ade3278829054c31d5a97
dctristero <123331986+dctristero@users.noreply.github.com>
2023-12-21 23:45:18 -0600

    responsive grid

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,23 +1,20 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import {
   ChakraProvider,
   Box,
   Flex,
   SimpleGrid,
   chakra,
-  Button
+  Button,
 } from "@chakra-ui/react";
 
 export default function Test() {
   // Function to generate an array of numbers from 1 to 81
-  const makeLinks = () => {
+  const makeNum = () => {
     const numbers = [];
     for (let i = 1; i <= 81; i++) {
-      numbers.push(
-        <Button color={["primary.300"]} className="navbtn" variant="ghost">
-          <Link to={`${i}`}>{i}</Link>
-        </Button>);
+      numbers.push(i);
     }
     return numbers;
   };
@@ -27,25 +24,45 @@ export default function Test() {
       <Flex
         bg={["primary.100"]}
         color={["primary.300"]}
-        p={7}
+        p={[".2vw"]}
         w="full"
-        h="full"
+        h="1000"
         justifyContent="center"
         alignItems="center"
         //pos="absolute"
       >
-        <Box p={2}>
-          <SimpleGrid id="grid" columns={9} spacing={0}>
-            {makeLinks().map((number) => (
-              <Box
-                key={number}
-                borderColor={["primary.300"]}
-                borderWidth="1px"
-                p={4}
-                textAlign="center"
-              >
-                {number}
-              </Box>
+        <Box p=".2vw">
+          <SimpleGrid
+            id="grid"
+            columns={9}
+            spacing={0}
+            borderColor={["primary.300"]}
+            borderWidth=".2vw"
+          >
+            {makeNum().map((number) => (
+                <Button
+                  as={Link}
+                  to={`${number}`}
+                  key={number}
+                  borderColor={["primary.300"]}
+                  borderWidth=".2vw"
+                  borderRadius="0"
+                  p={0}
+                  m={0}
+                  w={["10vw", "10vw", "6vw", "4vw"]}
+                  h={["10vw", "10vw", "6vw", "4vw"]}
+                  fontSize={["1em", "1.2em", "1.2em", "1.4em", "1.5em"]}
+                  // textAlign="center"
+                  // verticalAlign="center"
+                  // justifyContent="center"
+                  color={["primary.300"]}
+                  _hover={{ bg: "primary.200" }}
+                  className="navbtn"
+                  variant="ghost"
+                  pos="inherit"
+                >
+                  {number}
+                </Button>
             ))}
           </SimpleGrid>
         </Box>
