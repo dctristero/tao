613ab9b891c8287916915f612f3ebb326025418b
dctristero <123331986+dctristero@users.noreply.github.com>
2024-03-20 16:35:04 -0500

    nav and footer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import VersePage from "./pages/versePage";
 
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Navbar from "./components/navbar.js";
+import Navbar from "./components/navbar.js";
 
 // look at rachelreact for darkmode reference
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
         <Route path="/tao/" element={<Test />} />
         <Route path="/tao/:verseNumber" element={<VersePage />} />
