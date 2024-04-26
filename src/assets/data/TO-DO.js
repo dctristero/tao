6a4ce18ba020e518861be659d8e2660a9fa382f1
dctristero <123331986+dctristero@users.noreply.github.com>
2024-04-26 17:31:03 -0500

    nav and chinese responsiveness tweaks

---
+++ src/assets/data/TO-DO.js
@@ -0,0 +1,13 @@
+// make sure every page fills view vertically
+// fix grid
+// try customizing breakpoints, maybe combined with max width/height for buttons
+// make sure the grid is centered the same way as other components
+// center content on home page
+// change flex direction of home page content for widest view
+// make chinese float next to english on widest view
+// make style decisions about links on home and contact pages
+// may sure all font sizes feel good at all screen sizes
+// pick better light brown color
+
+
+// add search functionality
